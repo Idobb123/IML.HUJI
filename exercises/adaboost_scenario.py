c155e9e0ee7df9e7ede702c048abe161ebdac40c
idobenzvibrenner <ido.benzvibrenner@mail.huji.ac.il>
2023-06-05 12:48:15 +0300

    maybe done with Q2 for adaboost

--- exercises/adaboost_scenario.py
+++ exercises/adaboost_scenario.py
@@ -52,15 +52,27 @@ def fit_and_evaluate_adaboost(noise, n_learners=250, train_size=5000, test_size=
     Q1_fig = go.Figure([go.Scatter(x=iter_list, y=train_pred_list, name="train error"),
                        go.Scatter(x=iter_list, y=test_pred_list, name="test error")])
     Q1_fig.update_layout(title=f"Train and Test error of Adaboost learner as a function of number of learners<br>       "
-                               f"noise of {noise}",
+                               f"noise value of {noise}",
                          xaxis_title="number of learners",
                          yaxis_title="error")
-    Q1_fig.write_image(f"adaboost_{noise}.png")
+    Q1_fig.write_image(f"Q1_fig_with_noise_{noise}.png")
 
     # Question 2: Plotting decision surfaces
     T = [5, 50, 100, 250]
     lims = np.array([np.r_[train_X, test_X].min(axis=0), np.r_[train_X, test_X].max(axis=0)]).T + np.array([-.1, .1])
-    raise NotImplementedError()
+    Q2_fig = make_subplots(rows=2, cols=2, subplot_titles=[f"{T[i]} Iterations" for i in range(4)],
+                           horizontal_spacing=0.12, vertical_spacing=0.12)
+    symbols = np.where(test_y >= 0, "diamond", "cross")
+    for i in range(4):
+        partial_pred_func = lambda X: AD_model.partial_predict(X, T[i])
+        Q2_fig.add_traces([decision_surface(partial_pred_func, lims[0], lims[1], showscale=False),
+                          go.Scatter(x=test_X[:, 0], y=test_X[:, 1], mode="markers",
+                                     marker=dict(color=test_y, symbol=symbols, colorscale=[custom[0], custom[-1]],
+                                                 line=dict(color="black", width=1)))],
+                         rows=(i//2) + 1, cols=(i % 2) + 1)
+    Q2_fig.update_layout(title="Decision Boundary Of AdaBoost Ensemble As a Function Of Number Of Iterations",
+                         margin=dict(t=100), title_font=dict(size=16))
+    Q2_fig.write_image("Q2_fig.png")
 
     # Question 3: Decision surface of best performing ensemble
     raise NotImplementedError()
