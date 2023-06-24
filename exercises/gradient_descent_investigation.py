1f79ff0f88194bd4eeb661ae5915e154c7a6e98f
IdoBB <ido.benzvibrenner@mail.huji.ac.il>
2023-06-24 12:37:34 +0300

    finished with the exercise probably

--- exercises/gradient_descent_investigation.py
+++ exercises/gradient_descent_investigation.py
@@ -8,6 +8,8 @@ from IMLearn.desent_methods.modules import L1, L2
 from IMLearn.learners.classifiers.logistic_regression import LogisticRegression
 from IMLearn.utils import split_train_test
 from sklearn.metrics import roc_curve
+from IMLearn.model_selection import cross_validate
+from IMLearn.metrics import misclassification_error
 
 import plotly.graph_objects as go
 
@@ -97,17 +99,17 @@ def compare_fixed_learning_rates(init: np.ndarray = np.array([np.sqrt(2), np.e /
             model.fit(module(init), np.ndarray((0,)), np.ndarray((0,)))
             title = f"for {m_name} with \u03B7={eta}"
             module_dp_fig = plot_descent_path(module=module, descent_path=np.array(weights), title=title)
-            # module_dp_fig.write_image(rf"../figures/{m_name}_Descent_Path_for_eta={eta}.png")
-            module_dp_fig.write_image(rf"../test_directory/{m_name}_Descent_Path_for_eta={eta}.png")
+            module_dp_fig.write_image(rf"../figures/{m_name}_Descent_Path_for_eta={eta}.png")
+            # module_dp_fig.write_image(rf"../test_directory/{m_name}_Descent_Path_for_eta={eta}.png")
             conv_rate_fig.add_trace(go.Scatter(x=np.array(range(1, 1001)), y=values, name=f"\u03B7={eta}"))
             if values[-1] < best_loss:
                 best_loss = values[-1]
                 best_eta = eta
-        print(f"Best {m_name} loss is achieved with eta={best_eta} and its value is: {best_loss}")
+        print(f"Best {m_name} loss is achieved with eta={best_eta} and its value is: {np.round(best_loss,5)}")
         conv_rate_fig.update_layout(title=f"Convergence rate of {m_name} module for a selection of \u03B7 values",
                                     xaxis_title="Iterations", yaxis_title=f"{m_name} Norm value")
-        # conv_rate_fig.write_image(f"../figures/{m_name}_conv_fig.png")
-        conv_rate_fig.write_image(f"../test_directory/{m_name}_conv_fig.png")
+        conv_rate_fig.write_image(f"../figures/{m_name}_conv_fig.png")
+        # conv_rate_fig.write_image(f"../test_directory/{m_name}_conv_fig.png")
 
 
 def compare_exponential_decay_rates(init: np.ndarray = np.array([np.sqrt(2), np.e / 3]),
@@ -158,17 +160,60 @@ def load_data(path: str = "../datasets/SAheart.data", train_portion: float = .8)
 def fit_logistic_regression():
     # Load and split SA Heard Disease dataset
     X_train, y_train, X_test, y_test = load_data()
+    X_train, y_train, X_test, y_test = X_train.to_numpy(), y_train.to_numpy(), X_test.to_numpy(), y_test.to_numpy()
 
     # Plotting convergence rate of logistic regression over SA heart disease data
+    model = LogisticRegression(solver=GradientDescent(max_iter=20000, learning_rate=FixedLR(1e-4))).fit(X_train,
+                                                                                                        y_train)
+    fpr, tpr, thresholds = roc_curve(y_test, model.predict_proba(X_test))
+    # now selecting the best alpha
+    best_alpha_arg = np.argmax(tpr - fpr)
+    best_alpha = thresholds[best_alpha_arg]
 
+    # now fitting the model with the best alpha value:
+    model = LogisticRegression(solver=GradientDescent(max_iter=20000, learning_rate=FixedLR(1e-4)), alpha=best_alpha)\
+        .fit(X_train, y_train)
+    test_loss = np.round(model.loss(X_test, y_test), 3)
+
+    q8_fig = go.Figure(data=[go.Scatter(x=[0, 1], y=[0, 1], mode="lines", line=dict(color="black", dash='dash'),
+                                        name="Random Class Assignment"),
+              go.Scatter(x=fpr, y=tpr, mode='markers+lines', text=thresholds, name="", showlegend=False,
+                         hovertemplate="<b>Threshold:</b>%{text:.3f}<br>FPR: %{x:.3f}<br>TPR: %{y:.3f}"),
+                             go.Scatter(x=[fpr[best_alpha_arg]], y=[tpr[best_alpha_arg]], text=best_alpha,
+                                        mode="markers", marker=dict(size=14, color="orange", symbol="x"),
+                                        name="Optimal \u03B1 value")],
+        layout=go.Layout(title=f"ROC of Unregularized Logistic Regression Model over test set<br>Optimal \u03B1 value "
+                               f"of {np.round(best_alpha,3)} with test error of {test_loss}",
+                         xaxis=dict(title=r"$\text{False Positive Rate (FPR)}$"),
+                         yaxis=dict(title=r"$\text{True Positive Rate (TPR)}$"),
+                         margin=dict(t=100)))
+    q8_fig.write_image(f"../figures/q8_ROC_curve.png")
 
     # Fitting l1- and l2-regularized logistic regression models, using cross-validation to specify values
     # of regularization parameter
-    raise NotImplementedError()
+
+    lambda_values = np.array([0.001, 0.002, 0.005, 0.01, 0.02, 0.05, 0.1])
+    for penalty in ("l1", "l2"):
+        model_score_list = np.ndarray((lambda_values.shape[0]))
+        for i, lam in enumerate(lambda_values):
+            model = LogisticRegression(solver=GradientDescent(max_iter=20000, learning_rate=FixedLR(1e-4)),
+                                       penalty=penalty, lam=lam)
+            train_score, validation_score = cross_validate(model, X_train, y_train, misclassification_error)
+            model_score_list[i] = validation_score
+        opt_lambda = lambda_values[np.argmin(model_score_list)]
+
+        # now fitting model again and calculating the loss
+        opt_model = LogisticRegression(solver=GradientDescent(max_iter=20000, learning_rate=FixedLR(1e-4)),
+                                       penalty=penalty, lam=opt_lambda).fit(X_train, y_train)
+        opt_test_score = opt_model.loss(X_test, y_test)
+        print("\n")
+        print("optimal lambda value for " + penalty + " model: ", opt_lambda)
+        print("optimal test score for " + penalty + " model: ", np.round(opt_test_score, 5))
+        print("\n")
 
 
 if __name__ == '__main__':
     np.random.seed(0)
-    #compare_fixed_learning_rates()
+    compare_fixed_learning_rates()
     #compare_exponential_decay_rates()
     fit_logistic_regression()
